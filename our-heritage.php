b0ae8d4d5391fae17aaccf9279f2addc11782e8f
frontdev1701-neha <frontdev1701@gmail.com>
2026-01-22 18:11:40 +0530

    page complete of heritage page

--- our-heritage.php
+++ our-heritage.php
@@ -18,21 +18,55 @@
         </div>
     </div>
 </section>
-<section class="year-info text-center p-x mt-100">
+<section class="line-img-section mt-100">
     <img src="./images/our-heritage/line-img.png" alt="line img" class="line-img">
- 
-</section>
-<section>
+    <div class="year-info-section">
     <div class="year-wraper">
        <div class="year-info-item">
         <div class="year"><p class="title-24 text--white">2024</p></div>
         <div class="year-content">
             <h2 class="title-54">The Aesthetic Frontier</h2>
-            <p>Redefining Dermal Care</p>
+            <p class="text--para-alt mb-3">Redefining Dermal Care</p>
             <p>Launch of "Uncap"—our dedicated cosmeceutical channel. By applying pharmaceutical rigor to skincare, we began treating skin conditions at the cellular level, merging medical science with daily wellness.</p>
         </div>
     </div>
+       <div class="year-info-item">
+        <div class="year-content">
+            <h2 class="title-54">Scientific Diversification</h2>
+            <p class="text--para-alt mb-3">Beyond the Molecule</p>
+            <p>Our founders, Manish and Nilesh Dudhwewala, identified a critical need for high-stability chronic therapies in India. Alvio was established with a singular focus on Ethical Cardio-Diabetology.</p>
+        </div>
+         <div class="year"><p class="title-24 text--white">2022</p></div>
     </div>
+       <div class="year-info-item">
+         <div class="year"><p class="title-24 text--white">2020</p></div>
+        <div class="year-content">
+            <h2 class="title-54">The Reach of Healing</h2>
+            <p class="text--para-alt mb-3">Crossing Borders</p>
+            <p>Alvio expands its national presence to 16+ states and begins its international journey, exporting quality formulations to 11 countries. We became a trusted partner for over 35,000 Healthcare Professionals.</p>
+        </div>
+    </div>
+       <div class="year-info-item">
+        <div class="year-content">
+            <h2 class="title-54">Engineering Excellence</h2>
+            <p class="text--para-alt mb-3">The Standard of Purity</p>
+            <p>We established our state-of-the-art manufacturing facility in Roorkee, designed to meet stringent WHO-GMP and ISO standards. This milestone marked our transition into a self-reliant pharmaceutical manufacturer.</p>
+        </div>
+        <div class="year"><p class="title-24 text--white">2016</p></div>
+    </div>
+       <div class="year-info-item">
+        <div class="year"><p class="title-24 text--white">2012</p></div>
+        <div class="year-content">
+            <h2 class="title-54">The Genesis</h2>
+            <p class="text--para-alt mb-3">The Foundation of Alvio Pharmaceuticals.</p>
+            <p>Our founders, Manish and Nilesh Dudhwewala, identified a critical need for high-stability chronic therapies in India. Alvio was established with a singular focus on Ethical Cardio-Diabetology.</p>
+        </div>
+        
+    </div>
+    </div>
+</div>
+ 
 </section>
+
 <?php include 'top-footer.php'; ?>
 <?php include 'footer.php'; ?>
